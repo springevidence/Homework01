523e8ab7cef7fa9883dd49edf6d1b23b492e611c
springevidence <exshipbuilder1@gmail.com>
2023-08-08 11:53:30 +0300

    done HW11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -15,10 +15,17 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+
+    const change = (event: Event, value: number | number[]) => {
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
+
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
-
     return (
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
@@ -29,6 +36,9 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            onChange={change}
+                            value={value1}
+
                             // сделать так чтоб value1 изменялось // пишет студент
 
                         />
@@ -37,6 +47,8 @@ function HW11() {
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={change}
+                            value={[value1, value2]}
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
+// type SuperRangePropsType = {
+//     onChange: () => void
+//
+// }
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            color: '#8B8B8B', width: '147px', height: '4px'}}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
