7e45c4f6976ab3f5450609f73b75381c63579836
springevidence <exshipbuilder1@gmail.com>
2023-08-15 20:01:01 +0300

    done changing app theme homework

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,20 @@
 const initState = {
-    themeId: 1,
+    themeId: 2,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: changeThemeIdActionType): any => { // fix any
     switch (action.type) {
         // дописать
-
+        case "SET_THEME_ID":
+            return {...state, themeId: action.themeId}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): changeThemeIdActionType => ({ type: 'SET_THEME_ID', themeId: id }) // fix any
+
+type changeThemeIdActionType = {
+    type: 'SET_THEME_ID'
+    themeId: number
+}
